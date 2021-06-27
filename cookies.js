dedd30e3c633610a640a9965161e6e3c676eb503
hachad-14 <hachadsaad14@gmail.com>
2021-06-27 23:12:13 +0100

    test

--- cookies.js
+++ cookies.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     // If the 'hide cookie is not set we show the message
     if (!readCookie('hide')) {
-          $('#third-page').show();
+          $('#third-page').hide();
           console.log("cookies enregistrés")
     }
   
