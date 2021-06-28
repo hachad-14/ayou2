e8c573704e502dfb06f8510712493e9fe5ae3430
hachad-14 <hachadsaad14@gmail.com>
2021-06-28 21:53:22 +0100

    test

--- cookies.js
+++ cookies.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 
     // If the 'hide cookie is not set we show the message
     if (!readCookie('hide')) {
-      $('#txt').show();
+      $('#quizz').show();
     } else {
-        $('#txt').hide();
+        $('#quizz').hide();
         console.log("cookies présents");
       }
   
     // Add the event that closes the popup and sets the cookie that tells us to
     // not show it again until one day has passed.
     $('#close').click(function() {
-      $('#txt').hide();
+      $('#quizz').hide();
       console.log("cookies enregistrés");
       createCookie('hide', true, 1)
       return false;
