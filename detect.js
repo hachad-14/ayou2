3d1a4372230f37baf8d66998ed5d9ceaf1ccb18a
hachad-14 <hachadsaad14@gmail.com>
2021-07-25 17:15:28 +0100

    test

--- detect.js
+++ detect.js
@@ -1,9 +1,9 @@
-const countEl = document.getElementsByClassName('count');
+const countEl = document.getElementById('count');
 
 updateVisitCount();
 
 function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/ayou/jtm?amount=1')
+	fetch('https://api.countapi.xyz/update/ayou/jtm?amount=1 ')
 	.then(res => res.json())
 	.then(res => {
 		countEl.innerHTML = res.value;
